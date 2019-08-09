eaca7808a756752c4a5c5c7b7faad216b643709f
albanie <albanie@robots.ox.ac.uk>
2019-08-09 08:12:19 +0100

    update synching

--- misc/sync_checkpoints.py
+++ misc/sync_checkpoints.py
@@ -8,28 +8,36 @@ import argparse
 from pathlib import Path
 
 
-def sync_between_servers(save_dir, src_server, dest_server, local_tmp_dir, refresh,
-                         ckpt_list):
+def sync_between_servers(save_dir, src_server, dest_server, refresh, ckpt_list):
     with open(ckpt_list, "r") as f:
         ckpts = json.load(f)
 
-    filetypes = {
-        "log": ["info.log"],
-        "models": ["checkpoint-epoch100.pth", "config.json"]
-    }
-    for key, rel_dir in ckpts.items():
+    for key, subdict in ckpts.items():
+        rel_dir = subdict["timestamp"]
+        epoch = subdict["epoch"]
+
+        filetypes = {
+            "log": ["info.log"],
+            "models": [f"checkpoint-epoch{epoch}.pth", "config.json"]
+        }
 
         # copy experiment artifacts
         for filetype, fnames in filetypes.items():
             for fname in fnames:
                 if rel_dir.startswith("TODO"):
+                    print(f"Checkpoint (TODO) for {key}, skipping...")
                     continue
                 rel_path = Path(rel_dir) / fname
                 abs_path = Path(save_dir) / filetype / key / rel_path
                 print(f"{key} -> {abs_path} [{src_server} -> {dest_server}]")
+                # check if destination exists
+                exists = not os.system(f'ssh {dest_server} "test -f {str(abs_path)}"')
+                if exists and not refresh:
+                    print(f"found {abs_path} on dest server, skipping")
+                    continue
+
                 subprocess.call(["ssh", dest_server, "mkdir -p", str(abs_path.parent)])
-                sync_cmd = (f"scp -3 {src_server}:{abs_path} "
-                            f"{dest_server}:{abs_path}")
+                sync_cmd = f"scp -3 {src_server}:{abs_path} {dest_server}:{abs_path}"
                 print(f"running command {sync_cmd}")
                 os.system(sync_cmd)
 
@@ -40,22 +48,16 @@ if __name__ == "__main__":
     parser.add_argument("--ckpt_list", default="misc/server_checkpoints.json")
     parser.add_argument("--src_server", default="aws-albanie")
     parser.add_argument("--dest_server", default="gnodeb2")
-    parser.add_argument("--local_tmp_dir", default="/tmp/server-sync")
-    parser.add_argument("--refresh_server", action="store_true")
+    parser.add_argument("--refresh", action="store_true")
     parser.add_argument("--save_dir", default="~/data/exp/objectframe-pytorch/saved")
     args = parser.parse_args()
-
-    refresh_targets = {
-        "server": args.refresh_server,
-    }
 
     if args.action == "sync":
         sync_between_servers(
             save_dir=args.save_dir,
-            refresh=refresh_targets,
+            refresh=args.refresh,
             ckpt_list=args.ckpt_list,
             src_server=args.src_server,
-            local_tmp_dir=args.local_tmp_dir,
             dest_server=args.dest_server,
         )
     else:
