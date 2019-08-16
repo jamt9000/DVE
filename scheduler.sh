5e1b4f8b334bf90ebb1c3607e57769f77fe33ef9
albanie <albanie@robots.ox.ac.uk>
2019-08-16 15:19:48 +0100

    cleanup

--- scheduler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOGNAME="`date +'%Y-%m-%d_%H-%M-%S'`"
-WORKERS=2
-for GPU in $(seq 0 $((WORKERS-1)))
-do
-    echo $GPU
-    ./launcher.sh $LOGNAME $GPU $WORKERS &
-done
--- scratch_cmds.sh
+++
@@ -1,7 +0,0 @@
-ipy cache_feats.py -- --config configs/cache_feats_smallnet_mafl_64d_dve_128in_keypoints-ep57.json
-%run -i train.py --config configs/cached_smallnet_mafl_64d_dve_128in_keypoints-ep57-v1.json
-python train.py --config configs/cached_smallnet_mafl_64d_dve_128in_keypoints-ep57-v1.json --disable_workers 0 --profile 1 --disable_vis 1
-torch.cuda.empty_cache()
-python train.py --config configs/smallnet_mafl_64d_dve_128in_keypoints-ep57-v1-noup.json --disable_workers 0 --profile 0 --vis 0
-
-python3 train.py --config configs/smallnet_aflw_64d_dve_128in_keypoints-ep57-v1.json
